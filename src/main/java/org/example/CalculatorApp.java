1968864e55edb9f2f66fa2e77f3fdf80b52a6ca8
Nipuni-De-Silva <nipunisha274@gmail.com>
2024-05-24 12:20:13 +0530

    Implement Addition Functionality

--- src/main/java/org/example/CalculatorApp.java
+++ src/main/java/org/example/CalculatorApp.java
@@ -1,7 +1,34 @@
 package org.example;
 
+import java.util.Scanner;
 public class CalculatorApp {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+
+        double num1, num2;
+        double result = 0;
+
+        System.out.print("Enter Number 1: ");
+        num1 = sc.nextDouble();
+
+        System.out.print("Enter Number 2: ");
+        num2 = sc.nextDouble();
+
+        System.out.print("Enter the Operator (+,-,*,/) : ");
+        char operator = sc.next().charAt(0);
+
+        switch (operator){
+            // addition of two numbers
+            case '+':
+                result = num1 + num2;
+                break;
+
+            default:
+                System.out.println("You enter wrong input");
+        }
+
+
+        System.out.print("Final Result: " + num1 + " operator " + num2 + " = " + result);
+
     }
 }
